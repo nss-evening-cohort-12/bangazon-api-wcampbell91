5555286cb7e3e0bfca2b7849d2364dd66b8fb990
Billy Campbell <wcampbell91@gmail.com>
2021-02-11 20:47:42 -0600

    updated with try/except block

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -1,6 +1,7 @@
 """View module for handling requests about products"""
 import base64
 from django.core.files.base import ContentFile
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
@@ -102,8 +103,11 @@ class Products(ViewSet):
 
             new_product.image_path = data
 
-        new_product.clean_fields()
-        new_product.save()
+        try:
+            new_product.clean_fields()
+            new_product.save()
+        except ValidationError as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = ProductSerializer(
             new_product, context={'request': request})
@@ -184,7 +188,12 @@ class Products(ViewSet):
         product_category = ProductCategory.objects.get(pk=request.data["category_id"])
         product.category = product_category
 
-        product.clean_fields()
+        try:
+            product.clean_fields()
+            product.save()
+        except ValidationError as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
         product.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
