23ff94c64800b08e8e74569639def8804ec35182
Billy Campbell <wcampbell91@gmail.com>
2021-02-18 19:47:14 -0600

    Report for completed orders

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,9 +1,9 @@
-from bangazonreports.views.favorites_by_customer import customer_favorites_list
 from django.urls import path
-from .views import customer_favorites_list, products_over_1000, products_under_1000
+from .views import customer_favorites_list, products_over_1000, products_under_1000, completed_orders_list
 
 urlpatterns = [
     path('reports/favoritesellers', customer_favorites_list),
     path('reports/expensiveproducts', products_over_1000),
-    path('reports/inexpensiveproducts', products_under_1000)
+    path('reports/inexpensiveproducts', products_under_1000),
+    path('reports/completedorders', completed_orders_list)
 ]
